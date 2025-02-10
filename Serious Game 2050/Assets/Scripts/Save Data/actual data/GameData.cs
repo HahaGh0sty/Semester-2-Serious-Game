1d658f26d6773b7857538bd51c82370ee5d136e2
HahaGh0sty <ianvdwerf@hotmail.com>
2025-02-10 09:58:15 +0100

    addes saves stuff

---
+++ Serious Game 2050/Assets/Scripts/Save Data/actual data/GameData.cs
@@ -0,0 +1,15 @@
+[System.Serializable]
+public class GameData
+{
+    
+    
+        public int money;
+
+        //sets money to whatever value when u start new game
+        //game starts like this when no save data is available or you create a new save file
+        public GameData()
+        {
+            this.money = 0;
+        }
+    
+}
