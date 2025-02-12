1919c1fca0b982254322a5ad2878da1afc7d8a90
DannyChao06 <dannychao03@gmail.com>
2025-02-12 14:24:36 +0100

    Selected added to Grid Spaces. Grid spaces now are Children to their Parent Tile.

---
+++ Serious Game 2050/Assets/Scripts/GridSelect.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSelect : MonoBehaviour
+{
+    [SerializeField] private GameObject ThisGrid;
+    [SerializeField] public bool Selected = false;
+    [SerializeField] public bool Empty = false;
+
+    [SerializeField] Color s_MouseOverColor;
+    [SerializeField] Color s_OriginalColor;
+    [SerializeField] SpriteRenderer s_Renderer;
+
+    void Start()
+    {
+
+        ThisGrid = transform.gameObject;
+        s_MouseOverColor = new Color(0, 1, 0.3949249f, 0.7f);
+        s_Renderer = GetComponent<SpriteRenderer>();
+        s_OriginalColor = s_Renderer.material.color;
+    }
+
+    // Update is called once per frame
+    private void OnMouseOver()
+    {
+        if (Selected == false)
+        {
+            s_Renderer.material.color = s_MouseOverColor;
+
+            Debug.Log("Mouse on grid");
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (Selected == false)
+        {
+            s_Renderer.material.color = s_OriginalColor;
+            Debug.Log("Mouse off of");
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Selected == false)
+        {
+            Selected = true;
+        }
+        else if (Selected == true)
+        {
+            Selected = false;
+        }
+    }
+}
