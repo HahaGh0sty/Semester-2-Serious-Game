ad935cb020813d36c7544f41febde9a3b85dbfc4
HahaGh0sty <ianvdwerf@hotmail.com>
2025-02-12 12:43:31 +0100

    text stuff

---
+++ Serious Game 2050/Assets/Scripts/Resourcetext.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Resourcetext : MonoBehaviour
+{
+    private int moneycount = 0;
+
+    private TextMeshProUGUI Resourcecounttext;
+   
+    private void awake()
+    {
+        Resourcecounttext = this.GetComponent<TextMeshProUGUI>();
+
+    }
+
+   
+    private void update()
+    {
+        Resourcecounttext.text = "" + moneycount;
+    }
+
+
+    void OnMoneyGet()
+    {
+        moneycount++;
+    }
+}
