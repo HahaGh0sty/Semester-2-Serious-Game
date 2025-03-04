441dbe11c084ee5e9f528e86674a8776c9183e4f
HahaGh0sty <ianvdwerf@hotmail.com>
2025-03-04 09:38:31 +0100

    changed money button to work automatically instead of being a button and made it change both money and energy

--- Serious Game 2050/Assets/Scripts/moneybutton.cs
+++ Serious Game 2050/Assets/Scripts/moneybutton.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class moneybutton : MonoBehaviour, IDataPersistence
 {
     public int money;
+    public int energy;
 
-    public void OnButtonPress()
+    void Start()
+    {
+      InvokeRepeating("moneygain", 2f, 1f);
+    }
+
+    public void moneygain()
     {
       this.money += 1;
+      this.energy -= 1;
     }
 
     public void LoadData(GameData data)
     {
       this.money = data.money;
+      this.energy = data.energy;
     }
 
     public void SaveData(ref GameData data)
     {
       data.money = this.money;
+      data.energy = this.energy;
     }
-  public void Start()
-  {
-      
-  }
+ 
 }
