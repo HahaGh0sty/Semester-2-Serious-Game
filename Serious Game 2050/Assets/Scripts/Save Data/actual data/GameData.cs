6449d96977206e67fa7e481d2ade626fc89d6281
HahaGh0sty <ianvdwerf@hotmail.com>
2025-03-04 08:28:54 +0100

    added more types of resources to gamedata

--- Serious Game 2050/Assets/Scripts/Save Data/actual data/GameData.cs
+++ Serious Game 2050/Assets/Scripts/Save Data/actual data/GameData.cs
@@ -4,14 +4,24 @@ public class GameData
     
     
         public int money;
+        public int wood;
+        public int stone;
+        public int energy;
+        public int water;
+        public int vervuiling;
         
 
 
-        //sets money to whatever value when u start new game
+        //sets resources to whatever value when u start new game
         //game starts like this when no save data is available or you create a new save file
         public GameData()
         {
             this.money = 0;
+            this.wood = 0;
+            this.stone = 0;
+            this.energy = 0;
+            this.water = 0;
+            this.vervuiling = 0;
             
 
         }
