5e0e5f6172cd890ba219f197e4d11f0734e5e0d8
Luc-v-O <luc@vanogtrop-deruiter.nl>
2025-03-25 13:03:04 +0100

    gridsysteem werkt nu zoals het hoort

--- Serious Game 2050/Assets/TileChecker.cs
+++ Serious Game 2050/Assets/TileChecker.cs
@@ -1,42 +1,75 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+using System.Linq;
 
 public class TileChecker : MonoBehaviour
 {
-    public GameObject Grid;
+    public Tilemap tilemap;
+    public TileBase[] grassTiles, waterTiles, forestTiles;
+
+    private HashSet<TileBase> grassTilesSet;
+    private HashSet<TileBase> waterTilesSet;
+    private HashSet<TileBase> forestTilesSet;
+    private Vector3Int lastCheckedPosition;
+
     public bool tileMapGrassActive = false;
     public bool tileMapForestActive = false;
     public bool tileMapWaterActive = false;
 
     void Start()
     {
-        // Ensure at least one tile type is active
-        if (!tileMapGrassActive && !tileMapForestActive && !tileMapWaterActive)
+        grassTilesSet = new HashSet<TileBase>(grassTiles);
+        waterTilesSet = new HashSet<TileBase>(waterTiles);
+        forestTilesSet = new HashSet<TileBase>(forestTiles);
+
+        if (tilemap == null)
         {
-            Debug.Log("No active tile maps detected at start.");
+            tilemap = FindObjectOfType<Tilemap>(); // Automatically finds the first Tilemap in the scene
+        }
+
+        if (tilemap == null)
+        {
+            Debug.LogError("Tilemap not found! Make sure there is a Tilemap in the scene.");
         }
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void Update()
     {
-        CheckTile(collision.gameObject);
+        Vector3Int currentCell = tilemap.WorldToCell(transform.position);
+
+        if (currentCell != lastCheckedPosition) // Only check when position changes
+        {
+            lastCheckedPosition = currentCell;
+            CheckTile(currentCell);
+        }
     }
 
-    void CheckTile(GameObject obj)
+    void CheckTile(Vector3Int cellPosition)
     {
-        if (obj.CompareTag("IsWaterTile"))
-        {
-            tileMapWaterActive = true;
-            Debug.Log("Water Tile detected!");
-        }
-        else if (obj.CompareTag("IsForestTile"))
-        {
-            tileMapForestActive = true;
-            Debug.Log("Forest Tile detected!");
-        }
-        else if (obj.CompareTag("IsGrassTile"))
+        TileBase tile = tilemap.GetTile(cellPosition);
+        if (tile == null) return; // No tile, no need to check
+
+        if (grassTiles.Contains(tile))
         {
             tileMapGrassActive = true;
+            tileMapForestActive = false;
+            tileMapWaterActive = false;
             Debug.Log("Grass Tile detected!");
+        }
+        else if (waterTiles.Contains(tile))
+        {
+            tileMapWaterActive = true;
+            tileMapGrassActive = false;
+            tileMapForestActive = false;
+            Debug.Log("Water Tile detected!");
+        }
+        else if (forestTiles.Contains(tile))
+        {
+            tileMapForestActive = true;
+            tileMapGrassActive = false;
+            tileMapWaterActive = false;
+            Debug.Log("Forest Tile detected!");
         }
     }
 }
