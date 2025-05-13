57fc6e1f422f1c7e4431e28d2b07a05904e17469
HahaGh0sty <ianvdwerf@hotmail.com>
2025-05-13 08:42:45 +0200

    time manager is working

--- Serious Game 2050/Assets/Scripts/TimeManager.cs
+++ Serious Game 2050/Assets/Scripts/TimeManager.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeManager : MonoBehaviour
+public class TimeManager : MonoBehaviour, IDataPersistence
 {
+
+    public int CurrentYear;
+    public bool ArcadeMode;
     // Start is called before the first frame update
+    public void newGame()
+    {
+        if (ArcadeMode == true)
+        {
+            CurrentYear = 2025;
+        }
+        //if (ArcadeMode = 0)
+        //{
+            //CurrentYear = 
+       // }
+    }
     void Start()
     {
-        
+        //replace the time with 720 for full game, right now the 15 is purely for testing
+        InvokeRepeating("TimePass", 5f, 5f); 
+    }
+
+    void TimePass()
+    {
+        CurrentYear += 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if(CurrentYear = 2050)
+       // {
+            //gameover();
+       // }
+    }
+    public void LoadData(GameData data)
+    {
+        this.CurrentYear = data.CurrentYear;
+    }
+
+
+    public void SaveData(ref GameData data)
+    {
+        data.CurrentYear = this.CurrentYear;
     }
 }
