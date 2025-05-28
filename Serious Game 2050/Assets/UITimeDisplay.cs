0ef490c40c521e61a48874e0c951a3b28dc24d6b
HahaGh0sty <ianvdwerf@hotmail.com>
2025-05-28 15:03:16 +0200

    time now works with time manager, just doesn't display months yet

--- Serious Game 2050/Assets/UITimeDisplay.cs
+++ Serious Game 2050/Assets/UITimeDisplay.cs
@@ -4,26 +4,13 @@ using TMPro;
 public class TimeDisplay : MonoBehaviour
 {
     [SerializeField] private TMP_Text uiText;
-    [SerializeField] private UITimeManager timeManager;
+    public TimeManager timeManager;
 
 
     void Update()
     {
-        float seconds = timeManager.CurrentGameTime;
-        uiText.text = ConvertTime(seconds);
-    }
-
-    string ConvertTime(float seconds)
-    {
-        float timePerDay = timeManager.GetTimePerDay();
-        float timePerHour = timePerDay / 24f;
-        float timePerMinute = timePerHour / 60f;
-
-        int day = Mathf.FloorToInt(seconds / timePerDay) + 1;
-        float timeIntoDay = seconds % timePerDay;
-        int hour = Mathf.FloorToInt(timeIntoDay / timePerHour);
-        int minute = Mathf.FloorToInt((timeIntoDay % timePerHour) / timePerMinute);
-
-        return $"Dag {day}, {hour:00}:{minute:00} uur";
+        uiText.text = timeManager.CurrentYear.ToString();
+        
     }
 }
+
